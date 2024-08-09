6f37e8c710b4b2efe92b6baa92b7303782ec3d59
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-08-09 21:02:01 +0300

    Context sınıfı ve veritabanı oluşturuldu.

---
+++ CQRS/DAL/Context/CQRSContext.cs
@@ -0,0 +1,15 @@
+﻿using CQRS.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS.DAL.Context
+{
+    public class CQRSContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;initial catalog=CQRSDb;integrated security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
